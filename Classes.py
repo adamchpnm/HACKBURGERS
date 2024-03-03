6e0fafc2b11067be035abf8d55bd84d33f90102a
orpheus128 <danlindsay21@gmail.com>
2024-03-03 00:22:16 +0000

    mania

--- Classes.py
+++ Classes.py
@@ -10,6 +10,7 @@ class Script:
         self.script_id = script_id
         self.energy_criterion = energy_criterion
         self.battery_criterion = battery_criterion
+        # self.possible = []
 
     def criteria(self, energy=0, battery=0):
         if self.energy_criterion <= energy and self.battery_criterion <= battery:
@@ -17,60 +18,48 @@ class Script:
         else:
             return False
 
-
-class Event:
-
-    # Event takes in a dictionary of scripts which are the ways that a specific event can go
-    # It will facilitate transitions between scripts and also transition into an initial script depending
-    # on if the players stats
-    # if the player is statful enough to do everything, they get a choice
-    # the script object will handle all transitions to other scripts, but event starts with a starter script which will
-    # set in motion a series of scripts
+class StateMachine:
 
     def __init__(self, scripts: dict[Script: dict[int: Script]], player_stats):
         self.scripts = scripts
         self.player_states = player_stats
 
-    #you apply check scripts to check if a script can be moved to
+        # you apply check scripts to check if a script can be moved to
+
+
+    # change name to getBlock, clean, make it work for single states, not all states
     def checkScripts(self, player_stats, scripts: dict[Script: dict[int: Script]]):
         unavailable_scripts = []
-        for script in range(len(scripts)):
-            if scripts[script].criteria(player_stats["energy"], player_stats["battery"]):
+        for script in scripts:
+            if not scripts[script].criteria(player_stats["energy"], player_stats["battery"]):
                 unavailable_scripts.append(script)
         return unavailable_scripts
 
     # this function is basically just a nice way to display this
-    def transitionToScript(self, next_script: int):
+    # we dont give the player the choice to pick an unavailable event, so we do not have to validate inputs
+    def transitionToScript(self, next_script: Script):
         return next_script
 
     # this is what we give to block out the choices you CANT TAKE
-    def giveChoices(self, player_stats, scripts: dict[Script: dict[int : Script]]):
+    # could do blocking stuff in here
+    def removeChoices(self, player_stats, scripts: dict[Script: dict[int: Script]]):
         unavailable_scripts = self.checkScripts(player_stats, scripts)
 
+    def route(self, player_stats, scripts: dict[Script: dict[int: Script]], start):
+        game_running = True
+        current_script = scripts[start]
+        while game_running:
+            print("hiii")
+            # this will run the game loop, befire
 
-class StateMachine:
     player_stats = {"energy": 30, "battery": 30, "money": 0}
     test_script1 = Script(1)
     test_script2 = Script(2)
     test_script3 = Script(3)
 
-    # scripts will be stored in a triple nested dictionary!!!! basically the intuition is:
-    # each integer index will relate to one script in our entire program, which is the only key of the inner dictionary
+    # scripts will be stored in a double nested dictionary!!!! basically the intuition is:
+    # each script
     # the inner dictionary contains one value: the dictionary mapped to the script
-    scripts = {test_script1: {1: test_script2, 2: test_script3}, test_script2: {1: test_script2, 2:test_script3}}
+    scripts = {test_script1: {1: test_script2, 2: test_script3}, test_script2: {1: test_script2, 2: test_script3}}
 
-    start = Event(scripts, player_stats)
-    middle = Event(scripts, player_stats)
-    end = Event(scripts, player_stats)
 
-    events = {1: start, 2: middle, 3: end}
-    # transition function is more of a filter i think
-    current_state = events[1]
-
-    def setEvent(self, state: int):
-        self.current_state = state
-
-    def transitionToEvent(self, current_event: int, next_event: int, player_stats):
-        if current_event.criteria(player_stats["energy"], player_stats["battery"]):
-            self.setEvent(next_event)
-    # not sure what to do here yet reworking earlier parts
